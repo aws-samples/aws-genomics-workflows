29c5c83de01eee8a53bac112174b9fb17eb00a2a
Angel Pizarro <delagoya@gmail.com>
2018-05-12 23:24:40 -0400

    updated scripts

--- src/custom-ami-bootstrap.sh
+++ src/custom-ami-bootstrap.sh
@@ -1,8 +1,7 @@
-if [  "$#" -lt 4 ]; then
-  echo "USAGE: $0 <DEVICE NAME> <VOLUME GROUP NAME> <LOGICAL VOLUME NAME> <MOUNT POINT>"
+if [  "$#" -lt 3 ]; then
+  echo "USAGE: $0 <VOLUME GROUP NAME> <LOGICAL VOLUME NAME> <MOUNT POINT>"
   exit
 fi
-D=$1
 VG=$2
 LV=$3
 MP=$4
@@ -12,6 +11,27 @@ A=({a..z})
 Z=$(curl -s  http://169.254.169.254/latest/meta-data/placement/availability-zone )
 R=$(echo $Z | sed -e 's/[a-z]$//')
 I=$(curl -s  http://169.254.169.254/latest/meta-data/instance-id )
+N=$(ls /dev/xvd* | grep -v -E '[0-9]$' | wc -l)
+D="/dev/xvd${A[$N]}"
+
+# Create and attache the EBS Volume, also set it to delete on instance terminate
+V=$(aws ec2 create-volume --region $R --availability-zone $Z --volume-type gp2 --size 10 --encrypted --query "VolumeId" | sed 's/\"//g' )
+
+# await volume to become available
+until [ "$(aws ec2 describe-volumes --volume-ids $V --region $R --query "Volumes[0].State" | sed -e 's/\"//g')" == "available" ]; do
+  echo "Volume $V not yet available"
+  sleep 1
+done
+
+aws ec2 attach-volume --region $R --device $D --volume-id $V --instance-id $I
+# change the DeleteOnTermination volume attribute to true
+aws ec2 modify-instance-attribute --region $R --block-device-mappings "DeviceName=$D,Ebs={DeleteOnTermination=true,VolumeId=$V}" --instance-id $I
+
+# wait until device is available to start adding to PV
+until [ -b "$D" ]; do
+  echo "Volume $D not yet available"
+  sleep 1
+done
 
 # Register the physical volume
 pvcreate $D
