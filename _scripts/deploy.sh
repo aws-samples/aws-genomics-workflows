2ebbd976a1259add6e83d730b35c6e39eae43417
W. Lee Pang <wleepang@gmail.com>
2019-04-01 16:02:49 -0700

    use sync --delete for deploy

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -7,28 +7,28 @@ mkdocs build
 
 
 echo "publishing artifacts:"
-aws s3 cp \
+aws s3 sync \
     --profile asset-publisher \
     --acl public-read \
-    --recursive \
+    --delete \
     ./artifacts \
     s3://aws-genomics-workflows/artifacts
 
 
 echo "publishing templates:"
-aws s3 cp \
+aws s3 sync \
     --profile asset-publisher \
     --acl public-read \
-    --recursive \
+    --delete \
     --metadata commit=$(git rev-parse HEAD) \
     ./src/templates \
     s3://aws-genomics-workflows/templates
 
 
 echo "publishing site"
-aws s3 cp \
+aws s3 sync \
     --acl public-read \
-    --recursive \
+    --delete \
     ./site \
     s3://docs.opendata.aws/genomics-workflows
 
