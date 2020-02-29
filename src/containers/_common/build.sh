66ddd210c074821f5f1e20204151be3dcd10a84a
W. Lee Pang <wleepang@gmail.com>
2020-02-28 17:03:54 -0800

    use previous images as build caches

--- src/containers/_common/build.sh
+++ src/containers/_common/build.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
-
 set -e
 
 IMAGE_NAME=$1
 IMAGE_TAG=$2
 
+echo "Docker Login to ECR"
+eval $(aws ecr get-login --no-include-email --region ${AWS_REGION})
+
+# retrieve image layer cache from previously built build stage
+docker pull ${REGISTRY}/${IMAGE_NAME}:build-${IMAGE_TAG} || true
+
+# (re)build just the build stage of the image
+docker build \
+    --target build \
+    --cache-from ${REGISTRY}/${IMAGE_NAME}:build-${IMAGE_TAG} \
+    --build-arg VERSION=$IMAGE_TAG \
+    -t ${REGISTRY}/${IMAGE_NAME}:build-${IMAGE_TAG} .
+
 # build the base image
-docker build --build-arg VERSION=$IMAGE_TAG -t $IMAGE_NAME .
+docker build \
+    --cache-from ${REGISTRY}/${IMAGE_NAME}:build-${IMAGE_TAG} \
+    --build-arg VERSION=$IMAGE_TAG \
+    -t $IMAGE_NAME .
 
 # build the image with an AWS specific entrypoint
-docker build --build-arg BASE_IMAGE=$IMAGE_NAME -t $IMAGE_NAME:$IMAGE_TAG -t $IMAGE_NAME:latest -f _common/aws.dockerfile .
+docker build \
+    --build-arg BASE_IMAGE=$IMAGE_NAME \
+    -t $IMAGE_NAME:$IMAGE_TAG \
+    -t $IMAGE_NAME:latest \
+    -f _common/aws.dockerfile .
