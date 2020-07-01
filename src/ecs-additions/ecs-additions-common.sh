d3a9a3f3aea4d1a24ba2165248b1ca1902e96815
W. Lee Pang <wleepang@gmail.com>
2020-07-01 15:32:55 -0700

    make fetch_and_run a common resource

--- src/ecs-additions/ecs-additions-common.sh
+++ src/ecs-additions/ecs-additions-common.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 # add fetch and run batch helper script
-cd /opt/ecs-additions
-chmod a+x fetch_and_run.sh
-cp ./fetch_and_run.sh /usr/local/bin
+chmod a+x /opt/ecs-additions/fetch_and_run.sh
+cp /opt/ecs-additions/fetch_and_run.sh /usr/local/bin
