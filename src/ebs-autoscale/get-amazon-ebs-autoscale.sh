4c95b8c11a3d61d2bd541bd7aa297d273f6b1a68
W. Lee Pang <wleepang@gmail.com>
2020-07-01 16:15:22 -0700

    use awk to edit docker-storage options

--- src/ebs-autoscale/get-amazon-ebs-autoscale.sh
+++ src/ebs-autoscale/get-amazon-ebs-autoscale.sh
@@ -66,12 +66,18 @@ function install() {
             echo "Unsupported filesystem - $filesystem"
             exit 1
     esac
-    local docker_storage_options="s+OPTIONS=.*+OPTIONS=\"--storage-driver $docker_storage_driver\"+g"
-
+    local docker_storage_options="DOCKER_STORAGE_OPTIONS=\"--storage-driver $docker_storage_driver\""
+    
     cp -au /var/lib/docker /var/lib/docker.bk
     rm -rf /var/lib/docker/*
     sh /opt/amazon-ebs-autoscale/install.sh -f $filesystem -m /var/lib/docker > /var/log/ebs-autoscale-install.log 2>&1
-    sed -i $docker_storage_options /etc/sysconfig/docker-storage
+
+    awk -v docker_storage_options="$docker_storage_options" \
+        '{ sub(/DOCKER_STORAGE_OPTIONS=.*/, docker_storage_options); print }' \
+        /etc/sysconfig/docker-storage \
+        > /opt/amazon-ebs-autoscale/docker-storage
+    mv -f /opt/amazon-ebs-autoscale/docker-storage /etc/sysconfig/docker-storage
+
     cp -au /var/lib/docker.bk/* /var/lib/docker
 
 }
