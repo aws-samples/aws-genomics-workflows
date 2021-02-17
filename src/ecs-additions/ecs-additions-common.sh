3bf06f7d2b50bebfc7b89d3e276e7e388e6f30a3
Henrique Silva <henrique.rd.silva@gmail.com>
2021-02-17 11:46:04 -0300

    move swap declaration to ecs-additions-common.sh

--- src/ecs-additions/ecs-additions-common.sh
+++ src/ecs-additions/ecs-additions-common.sh
@@ -3,3 +3,11 @@
 # add fetch and run batch helper script
 chmod a+x /opt/ecs-additions/fetch_and_run.sh
 cp /opt/ecs-additions/fetch_and_run.sh /usr/local/bin
+
+# add 4GB of swap space
+dd if=/dev/zero of=/swapfile bs=128M count=32
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+swapon -s
+echo '/swapfile swap swap defaults 0 0' >> /etc/fstab
