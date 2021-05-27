7367f5193489bc01a3ec2fe617ce78ef9de0b1ee
Henrique Silva <henrique.rd.silva@gmail.com>
2021-05-27 12:45:21 +0100

    change secret name and scope down permissions

--- src/ecs-additions/ecs-additions-cromwell.sh
+++ src/ecs-additions/ecs-additions-cromwell.sh
@@ -2,10 +2,10 @@
 
 SECRET_EXIST=$(
   aws secretsmanager list-secrets \
-    --filters "Key=name,Values=CROMWELL_DOCKERHUB_CREDENTIALS" | jq '.SecretList | length > 0')
+    --filters "Key=name,Values=cromwell/credentials/dockerhub" | jq '.SecretList | length > 0')
 
 if [[ "$SECRET_EXIST" = true ]]; then
-  SECRET_STRING=$(aws secretsmanager get-secret-value --secret-id CROMWELL_DOCKERHUB_CREDENTIALS --query SecretString --output text)
+  SECRET_STRING=$(aws secretsmanager get-secret-value --secret-id cromwell/credentials/dockerhub --query SecretString --output text)
   echo 'ECS_ENGINE_AUTH_TYPE=docker' >>/etc/ecs/ecs.config
   echo 'ECS_ENGINE_AUTH_DATA={"https://index.docker.io/v1/":'${SECRET_STRING}'}' >>/etc/ecs/ecs.config
 fi
