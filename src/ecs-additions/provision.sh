5e2438bd91de661823515f8a339ca3d217b2db66
Alexis Lucattini <alexis.lucattini@umccr.org>
2021-11-18 09:39:45 +1100

    Remove and reinstall latest versions of amazon ssm agent for alinux2
    * Also completed a general cleanup of the script (shellcheck was throwing a few errors)
    * Add consistency to if statements
    * Run shell scripts with 'bash' shebang through bash NOT sh. 
    * evaluate and export separately env vars separately

--- src/ecs-additions/provision.sh
+++ src/ecs-additions/provision.sh
@@ -3,8 +3,10 @@
 set -e
 set -x
 
-export OS=`uname -r`
-BASEDIR=`dirname $0`
+OS="$(uname -r)"
+BASEDIR="$(dirname "${0}")"
+
+export OS
 
 # Expected environment variables
 #   GWFCORE_NAMESPACE
@@ -17,10 +19,28 @@ printenv
 if [[ $OS =~ "amzn1" ]]; then
     start amazon-ssm-agent
 elif [[ $OS =~ "amzn2" ]]; then
-    systemctl enable amazon-ssm-agent
-    systemctl start amazon-ssm-agent
+    echo "Stopping and upgrading amazon ssm agent" 1>&2
+    systemctl stop amazon-ssm-agent
+    systemctl disable amazon-ssm-agent
+    echo "Downloading latest version" 1>&2
+    curl \
+      --output "amazon-ssm-agent.rpm" \
+      "https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm"
+    echo "Upgrading ssm agent to latest version" 1>&2
+    rpm \
+      --quiet \
+      --install \
+      --force \
+      --upgrade \
+      --replacepkgs \
+      "amazon-ssm-agent.rpm"
+    echo "Re-enabling amazon ssm agent" 1>&2
+    systemctl enable --output=verbose amazon-ssm-agent
+    systemctl start --output=verbose amazon-ssm-agent
+    echo "Cleaning up" 1>&2
+    rm "amazon-ssm-agent.rpm"
 else
-    echo "unsupported os: $os"
+    echo "unsupported os: ${OS}"
     exit 100
 fi
 
@@ -51,7 +71,7 @@ function ecs() {
                 ;;
         esac
     else
-        echo "unsupported os: $os"
+        echo "unsupported os: ${OS}"
         exit 100
     fi
 }
@@ -66,38 +86,40 @@ set -e
 
 ARTIFACT_S3_ROOT_URL=$(\
     aws ssm get-parameter \
-        --name /gwfcore/${GWFCORE_NAMESPACE}/installed-artifacts/s3-root-url \
+        --name "/gwfcore/${GWFCORE_NAMESPACE}/installed-artifacts/s3-root-url" \
         --query 'Parameter.Value' \
         --output text \
 )
 
 ORCHESTRATOR_EXIST=$(\
     aws ssm describe-parameters \
-        --filters "Key=Name,Values=/gwfcore/${GWFCORE_NAMESPACE}/orchestrator" | jq '.Parameters | length > 0')
+        --filters "Key=Name,Values=/gwfcore/${GWFCORE_NAMESPACE}/orchestrator" | \
+    jq '.Parameters | length > 0' \
+)
 
-if [[ "$ORCHESTRATOR_EXIST" = true ]]
-then
+if [[ "$ORCHESTRATOR_EXIST" == "true" ]]; then
     WORKFLOW_ORCHESTRATOR=$(\
         aws ssm get-parameter \
-            --name /gwfcore/${GWFCORE_NAMESPACE}/orchestrator \
+            --name "/gwfcore/${GWFCORE_NAMESPACE}/orchestrator" \
             --query 'Parameter.Value' \
-            --output text)
+            --output text \
+    )
 fi
 
 # retrieve and install amazon-ebs-autoscale
 cd /opt
-sh $BASEDIR/get-amazon-ebs-autoscale.sh \
+bash "${BASEDIR}/get-amazon-ebs-autoscale.sh" \
     --install-version dist_release \
-    --artifact-root-url $ARTIFACT_S3_ROOT_URL \
+    --artifact-root-url "${ARTIFACT_S3_ROOT_URL}" \
     --file-system btrfs
 
 # common provisioning for all workflow orchestrators
 cd /opt
-sh $BASEDIR/ecs-additions-common.sh
+bash "${BASEDIR}/ecs-additions-common.sh"
 
 # workflow specific provisioning if needed
-if [[ $WORKFLOW_ORCHESTRATOR ]]; then
-    if [ -f "$BASEDIR/ecs-additions-$WORKFLOW_ORCHESTRATOR.sh" ]; then
-        sh $BASEDIR/ecs-additions-$WORKFLOW_ORCHESTRATOR.sh
+if [[ -n "$WORKFLOW_ORCHESTRATOR" ]]; then
+    if [[ -f "$BASEDIR/ecs-additions-$WORKFLOW_ORCHESTRATOR.sh" ]]; then
+        bash "$BASEDIR/ecs-additions-$WORKFLOW_ORCHESTRATOR.sh"
     fi
 fi
